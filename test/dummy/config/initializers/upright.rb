61a46ec35482a249c2e746e55c71f1767190ec01
Lewis Buckley <lewis@37signals.com>
2026-01-19 11:18:46 +0000

    Fix test suite

--- test/dummy/config/initializers/upright.rb
+++ test/dummy/config/initializers/upright.rb
@@ -1,5 +1,6 @@
 Upright.configure do |config|
   config.sites_config_path = Rails.root.join("config/upright/sites.yml")
+  config.probes_path = Rails.root.join("config/upright/probes")
   config.frozen_record_path = Rails.root.join("config")
   config.user_agent = "Upright-Test/1.0"
 end
