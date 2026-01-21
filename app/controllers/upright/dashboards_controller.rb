9c513090fc3b95186e1e6275178952dfcb70ec43
Lewis Buckley <lewis@37signals.com>
2026-01-21 11:30:48 +0000

    Fix constant lookup

--- app/controllers/upright/dashboards_controller.rb
+++ app/controllers/upright/dashboards_controller.rb
@@ -2,6 +2,6 @@ class Upright::DashboardsController < Upright::ApplicationController
   def uptime
     @sites = Upright.sites
     @probe_type = params.fetch(:probe_type, "http")
-    @report = UptimeReport.new(site_code: params[:site_code], probe_type: @probe_type)
+    @report = Upright::UptimeReport.new(site_code: params[:site_code], probe_type: @probe_type)
   end
 end
