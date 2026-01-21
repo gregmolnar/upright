21d71dfedee7dafc507ec94f658ec11b53b398d4
Lewis Buckley <lewis@37signals.com>
2026-01-21 10:30:58 +0000

    Refactor CSS into multiple files

--- app/helpers/upright/application_helper.rb
+++ app/helpers/upright/application_helper.rb
@@ -1,2 +1,9 @@
 module Upright::ApplicationHelper
+  def upright_stylesheet_link_tag(**options)
+    stylesheets = Dir[Upright::Engine.root.join("app/assets/stylesheets/upright/*.css")]
+      .sort
+      .map { |f| "upright/#{File.basename(f, '.css')}" }
+
+    stylesheet_link_tag(*stylesheets, **options)
+  end
 end
