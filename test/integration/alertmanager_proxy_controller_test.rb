e09c707f60397c3ab0032d03bd05caac4c434b9c
Lewis Buckley <lewis@37signals.com>
2026-01-23 11:42:45 +0000

    No need to stub envs

--- test/integration/alertmanager_proxy_controller_test.rb
+++ test/integration/alertmanager_proxy_controller_test.rb
@@ -4,11 +4,10 @@ require "webmock/minitest"
 class AlertmanagerProxyControllerTest < ActionDispatch::IntegrationTest
   setup do
     on_subdomain :app
-    ENV["ALERTMANAGER_URL"] = "http://upright-alertmanager:9093"
   end
 
   test "proxies requests when authenticated" do
-    stub_request(:get, "http://upright-alertmanager:9093/")
+    stub_request(:get, "http://localhost:9093/")
       .to_return(status: 200, body: "Alertmanager UI")
 
     sign_in
--- test/integration/prometheus_proxy_controller_test.rb
+++ test/integration/prometheus_proxy_controller_test.rb
@@ -4,12 +4,11 @@ require "webmock/minitest"
 class PrometheusProxyControllerTest < ActionDispatch::IntegrationTest
   setup do
     on_subdomain :app
-    ENV["PROMETHEUS_URL"] = "http://upright-prometheus:9090"
     ENV["PROMETHEUS_OTLP_TOKEN"] = "test-token"
   end
 
   test "proxies requests when authenticated" do
-    stub_request(:get, "http://upright-prometheus:9090/graph").to_return(status: 200, body: "Prometheus UI")
+    stub_request(:get, "http://localhost:9090/graph").to_return(status: 200, body: "Prometheus UI")
     sign_in
 
     get "/prometheus/graph"
@@ -18,7 +17,7 @@ class PrometheusProxyControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "OTLP endpoint accepts valid token" do
-    stub_request(:post, "http://upright-prometheus:9090/api/v1/otlp/v1/metrics").to_return(status: 200)
+    stub_request(:post, "http://localhost:9090/api/v1/otlp/v1/metrics").to_return(status: 200)
 
     post "/prometheus/api/v1/otlp/v1/metrics",
       headers: { "Authorization" => "Bearer test-token", "Content-Type" => "application/x-protobuf" }
