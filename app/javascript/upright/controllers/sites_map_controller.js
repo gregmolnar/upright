48aa323997a2980b262a26d7a196e4b8c413e4f5
Lewis Buckley <lewis@37signals.com>
2026-01-24 18:24:50 +0000

    Clarify map controller

--- app/javascript/upright/controllers/sites_map_controller.js
+++ app/javascript/upright/controllers/sites_map_controller.js
@@ -6,15 +6,8 @@ export default class extends Controller {
 
   connect() {
     const map = L.map(this.element, { scrollWheelZoom: false }).setView([30, 0], 2)
-    const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches
 
-    const tiles = isDark
-      ? "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-      : "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-
-    L.tileLayer(tiles, {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
-    }).addTo(map)
+    L.tileLayer(this.tileUrl, { attribution: this.attribution }).addTo(map)
 
     for (const site of this.sitesValue) {
       if (!site.lat || !site.lon) continue
@@ -26,5 +19,15 @@ export default class extends Controller {
         .on("mouseout", function() { this.closePopup() })
         .on("click", () => window.location.href = site.url)
     }
+  }
+
+  get tileUrl() {
+    return window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+      : "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  }
+
+  get attribution() {
+    return '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
   }
 }
