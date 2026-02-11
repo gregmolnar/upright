c4d09050c74c06515086e0720faae8dded8b3751
Lewis Buckley <lewis@37signals.com>
2026-02-11 14:14:11 +0000

    Simplify auto-refresh controller

--- app/javascript/upright/controllers/auto_refresh_controller.js
+++ app/javascript/upright/controllers/auto_refresh_controller.js
@@ -4,42 +4,13 @@ export default class extends Controller {
   static values = { interval: { type: Number, default: 60000 } }
 
   connect() {
-    this.startRefreshing()
-    document.addEventListener("visibilitychange", this.handleVisibilityChange)
+    this.timer = setInterval(() => {
+      this.element.src = window.location.href
+      this.element.reload()
+    }, this.intervalValue)
   }
 
   disconnect() {
-    this.stopRefreshing()
-    document.removeEventListener("visibilitychange", this.handleVisibilityChange)
-  }
-
-  startRefreshing() {
-    this.timer = setInterval(() => this.refresh(), this.intervalValue)
-  }
-
-  stopRefreshing() {
-    if (this.timer) {
-      clearInterval(this.timer)
-      this.timer = null
-    }
-  }
-
-  refresh() {
-    if (this.element.tagName === "TURBO-FRAME" && this.element.src) {
-      this.element.reload()
-    } else if (this.element.tagName === "TURBO-FRAME") {
-      // Frame without explicit src reloads from current URL
-      this.element.src = window.location.href
-      this.element.reload()
-    }
-  }
-
-  handleVisibilityChange = () => {
-    if (document.hidden) {
-      this.stopRefreshing()
-    } else {
-      this.refresh()
-      this.startRefreshing()
-    }
+    clearInterval(this.timer)
   }
 }
